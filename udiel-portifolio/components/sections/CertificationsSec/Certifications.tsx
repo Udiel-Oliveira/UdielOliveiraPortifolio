ee8300d98415e5e72be6e66bfda09057d6beb927
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2026-03-27 19:30:55 -0300

    Feat: adicionando componente CursoSwiper e ajustando estilos da seção de certificações

--- udiel-portifolio/components/sections/CertificationsSec/Certifications.tsx
+++ udiel-portifolio/components/sections/CertificationsSec/Certifications.tsx
@@ -1,18 +1,10 @@
+import CursoSwiper from "@/components/ui/CursoSwiper/CursosSwiper";
 import Styles from "./Certifications.module.css";
 
 export default function Certifications() {  
     return (
         <section id="certifications" className={Styles.section}>
-            <h1 className={Styles.title}>Certifications</h1>
-            <p className={Styles.description}>
-                I have obtained several certifications in the field of software development, including:
-            </p>
-            <ul className={Styles.list}>
-                <li>Certified JavaScript Developer - XYZ Institute</li>
-                <li>Full Stack Web Development Certification - ABC Academy</li>
-                <li>React.js Certification - DEF Online Course</li>
-                <li>Node.js Certification - GHI Training Center</li>
-            </ul>
+<CursoSwiper/>
         </section>
     );
 }
