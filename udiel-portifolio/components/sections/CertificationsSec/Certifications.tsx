52002810875746f638ca62208259ef04d28221f1
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2026-03-28 19:49:12 -0300

    Styles: aprimorando a seção de certificações com novos estilos

--- udiel-portifolio/components/sections/CertificationsSec/Certifications.tsx
+++ udiel-portifolio/components/sections/CertificationsSec/Certifications.tsx
@@ -1,10 +1,13 @@
 import CursoSwiper from "@/components/ui/CursoSwiper/CursosSwiper";
 import Styles from "./Certifications.module.css";
+import Button from "@/components/ui/Button/Button";
+import { Trophy } from "akar-icons";
 
 export default function Certifications() {  
     return (
         <section id="certifications" className={Styles.section}>
-<CursoSwiper/>
+            <Button icon={<Trophy/>}>Certificações</Button>
+            <CursoSwiper/>
         </section>
     );
 }
--- udiel-portifolio/components/ui/CursoSwiper/CursosSwiper.tsx
+++ udiel-portifolio/components/ui/CursoSwiper/CursosSwiper.tsx
@@ -1,14 +1,21 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
-
+import { useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import Styles from "./CursosSwiper.module.css";
 import Button from "../Button/Button";
-import { Info } from "akar-icons";
+import {
+  ArrowUpRight,
+  Calendar,
+  Cross,
+  Diamond,
+  Enlarge,
+  Info,
+} from "akar-icons";
 
 // Importa o JSON
 import cursosObj from "@/data/courses.json";
@@ -16,10 +23,15 @@ import cursosObj from "@/data/courses.json";
 const cursos = Object.values(cursosObj);
 
 // separa por destaque
-const cursosDestaque = cursos.filter(curso => curso.destaque);
-const cursosRestantes = cursos.filter(curso => !curso.destaque);
+const cursosDestaque = cursos.filter((curso) => curso.destaque);
+const cursosRestantes = cursos.filter((curso) => !curso.destaque);
 
 export default function CursoSwiper() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+  const [isInfoOpen, setIsInfoOpen] = useState(false);
+  const [selectedCursoInfo, setSelectedCursoInfo] = useState(null);
+
   return (
     <div className={Styles.cursoSwiperContainer}>
       {/* Swiper dos destaques */}
@@ -29,22 +41,28 @@ export default function CursoSwiper() {
         spaceBetween={30}
         navigation={true}
         pagination={{ clickable: true }}
-        modules={[Autoplay,Navigation, Pagination]}
+        modules={[Autoplay, Navigation, Pagination]}
         loop={true}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
         className={Styles.swiper}
       >
         {cursosDestaque.map((curso, i) => (
           <SwiperSlide key={i} className={Styles.swiperSlide}>
             <img src={curso.imagem} alt={curso.nome} className={Styles.image} />
             <div className={Styles.action}>
-              <Button icon={<Info />} href={curso.link} target="_blank">
-                Detalhes
-              </Button>
+              <Button
+                icon={<Enlarge />}
+                onClick={() => {
+                  setSelectedImage(curso.imagem);
+                  setSelectedCursoInfo(curso);
+                  setIsModalOpen(true);
+                }}
+                className={Styles.enlargeButton}
+              ></Button>
             </div>
           </SwiperSlide>
         ))}
@@ -69,16 +87,112 @@ export default function CursoSwiper() {
         >
           {cursosRestantes.map((curso, i) => (
             <SwiperSlide key={i} className={Styles.slideRow}>
-              <img src={curso.imagem} alt={curso.nome} className={Styles.image} />
-              <div className={Styles.action}>
-                <Button icon={<Info />} href={curso.link} target="_blank">
-                  Detalhes
-                </Button>
-              </div>
+              <img
+                src={curso.imagem}
+                alt={curso.nome}
+                className={Styles.image}
+                onClick={() => {
+                  setSelectedImage(curso.imagem);
+                  setSelectedCursoInfo(curso);
+                  setIsModalOpen(true);
+                }}
+              />
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
+
+      {/* Modal principal */}
+      {isModalOpen && (
+        <div className={Styles.overlay} onClick={() => setIsModalOpen(false)}>
+          <div className={Styles.modal} onClick={(e) => e.stopPropagation()}>
+            <div className={Styles.modalImage}>
+              <img
+                src={selectedImage}
+                alt="Full screen"
+                className={Styles.fullImage}
+              />
+              <div className={Styles.action}>
+                <Button
+                  icon={<Info />}
+                  onClick={() => setIsInfoOpen(true)}
+                  className={Styles.detailButton}
+                >
+                  Detalhes
+                </Button>
+              </div>
+            </div>
+            <Button
+              className={Styles.closeButton}
+              onClick={() => {
+                setIsModalOpen(false);
+                setIsInfoOpen(false);
+                setSelectedCursoInfo(null);
+                setSelectedImage("");
+              }}
+              icon={<Cross />}
+              variant="primary"
+            />
+            {isInfoOpen && selectedCursoInfo && (
+              <div
+                onClick={() => setIsInfoOpen(false)}
+                className={Styles.modalInfoContainer}
+              >
+                <div className={Styles.modalInfoContent}>
+                  <div className={Styles.modalHeader}>
+                    <Info />
+                    <h2>Detalhes</h2>
+                  </div>
+                  <h1 className={Styles.modalTitle}>
+                    {selectedCursoInfo.nome}
+                  </h1>
+                  <p className={Styles.modalDescription}>
+                    {selectedCursoInfo.descricao}
+                  </p>
+                  <div className={Styles.detailsInfo}>
+                    <p>
+                      <strong>Instituição:</strong>{" "}
+                      <Button icon={<Diamond />} className={Styles.badgeCurso}>
+                        {selectedCursoInfo.instituicao}
+                      </Button>
+                    </p>
+                    <p>
+                      <strong>Data</strong>
+                      <Button icon={<Calendar />} className={Styles.badgeCurso}>
+                        {selectedCursoInfo.data}
+                      </Button>
+                    </p>
+                    <p>
+                      <strong>Status:</strong>
+                      <Button className={Styles.badgeCurso}>
+                        {selectedCursoInfo.status}
+                      </Button>
+                    </p>
+                  </div>
+                </div>
+                <div className={Styles.detailsActions}>
+                  <Button
+                    icon={<Cross />}
+                    onClick={() => {
+                      setIsInfoOpen(false);
+                    }}
+                    className={Styles.closeButtonDetailInfo}
+                  >
+                    Fechar
+                  </Button>
+                  <Button
+                    href={selectedCursoInfo.link}
+                    icon={<ArrowUpRight />}
+                    target="_blank"
+                  >
+                    Link Certificado
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
