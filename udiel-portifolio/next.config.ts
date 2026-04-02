90327c70b6edee6e16bc74d450b4b25b319ff4f6
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2026-04-02 20:27:38 -0300

    Feat: adicionar favicon, atualizar metadados e otimizar responsividade do layout

--- udiel-portifolio/next.config.ts
+++ udiel-portifolio/next.config.ts
@@ -1,7 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "tse1.mm.bing.net",
+      "portal.redevix.com",
+      "agoranovale.com.br",
+      "cdn-icons-png.flaticon.com"
+    ],
+  },
 };
 
 export default nextConfig;
